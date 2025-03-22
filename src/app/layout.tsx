2ed685e4fe58137e85e0d55f6f395adc3e52df06
Henry Li <henry1943@163.com>
2025-03-22 17:17:03 +0800

    chore: add emoji

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { type Metadata } from "next";
 import "~/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: "LangManus",
+  title: "🦜🤖 LangManus",
   description:
     "A community-driven AI automation framework that builds upon the incredible work of the open source community.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
